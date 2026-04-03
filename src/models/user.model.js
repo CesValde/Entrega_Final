76323fb6575012b2932fa817cce89b85c7e7a928
CesValde <valderramacesar2002@gmail.com>
2026-04-03 17:42:21 -0300

    add: model
    user

---
+++ src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+   {
+      first_name: {
+         type: String,
+         required: true
+      },
+      last_name: {
+         type: String,
+         required: true
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true
+      },
+      password: {
+         type: String,
+         required: true
+      },
+      role: {
+         type: String,
+         enum: ["user", "admin"],
+         default: "user"
+      },
+      pets: {
+         type: [],
+         default: []
+      },
+
+      documents: {
+         type: [
+            {
+               name: String,
+               reference: String
+            }
+         ],
+         default: []
+      },
+      last_connection: {
+         type: Date,
+         default: Date.now
+      }
+   },
+   {
+      timestamps: true
+   }
+)
+
+const UserModel = mongoose.model("users", userSchema)
+
+export default UserModel
